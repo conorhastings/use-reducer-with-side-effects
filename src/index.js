4849cd7629a641632831b7b4e2ffbb03efa19710
Conor Hastings <hastings.conorm@gmail.com>
2019-06-10 17:41:05 -0700

    rewrite

--- src/index.js
+++ src/index.js
@@ -1,46 +1,62 @@
-import { useState, useMemo } from "react";
+import { useReducer, useEffect } from "react";
 
-export const Update = newState => ({ setState }) => {
-  setState(newState);
+export const Update = newState => ({ newState });
 
-  return newState;
-};
+export const NoUpdate = () => ({ noUpdate: true });
 
-export const NoUpdate = () => ({ state }) => state;
+export const UpdateWithSideEffect = (newState, newSideEffect) => ({
+  newState,
+  newSideEffect: { effect: newSideEffect, id: Math.random() * 10000 }
+});
 
-export const UpdateWithSideEffect = (newState, sideEffect) => ({
-  state,
-  setState,
-  reducer
-}) => {
-  const updatedState = Update(newState)({ setState });
+export const SideEffect = newSideEffect => ({
+  newSideEffect: { effect: newSideEffect, id: Math.random() * 10000 }
+});
 
-  sideEffect(newState, action => {
-    const reducerReturn = reducer(newState, action);
-    reducerReturn({ state: updatedState, setState, reducer });
-  });
-};
+async function executeSideEffects({ sideEffects, state, dispatch }) {
+  await Promise.all(
+    sideEffects.forEach(sideEffect => {
+      sideEffect.effect(state, dispatch);
+      dispatch({
+        type: "REMOVE_EXECUTED_SIDE_EFFECT",
+        sideEffect
+      });
+    })
+  );
+}
 
-export const SideEffect = sideEffect => ({ state, reducer, setState }) =>
-  sideEffect(state, action => {
-    const reducerReturn = reducer(state, action);
-    reducerReturn({ state, setState, reducer });
-  });
+function finalReducer(reducer) {
+  return function({ state, sideEffects = [] }, action) {
+    let { newState, newSideEffect, noUpdate } = reducer(state, action);
+    let newSideEffects = sideEffects;
+    newSideEffects = newSideEffect
+      ? [...newSideEffects, newSideEffect]
+      : newSideEffects;
+    if (action.type === "REMOVE_EXECUTED_SIDE_EFFECT") {
+      newSideEffects = sideEffects.filter(
+        effect => effect.id !== action.sideEffect.id
+      );
+    } else if (noUpdate) {
+      return { state, sideEffects };
+    } else if (newSideEffect) {
+      sideEffects = (sideEffects || []).concat(newSideEffect);
+    }
+    return { state: newState || state, sideEffects: newSideEffects };
+  };
+}
 
 export default function useCreateReducerWithEffect(reducer, initialState) {
-  const [state, setState] = useState(initialState);
-
-  const dispatch = useMemo(
-    () => action => {
-      const reducerReturn = reducer(state, action);
-      if (typeof reducerReturn === "function") {
-        reducerReturn({ state, setState, reducer });
-      } else {
-        setState(reducerReturn);
+  const [{ state, sideEffects }, dispatch] = useReducer(finalReducer(reducer), {
+    state: initialState,
+    sideEffects: []
+  });
+  useEffect(() => {
+    if (sideEffects.length) {
+      async function runSideEffects() {
+        await executeSideEffects({ sideEffects, state, dispatch });
       }
-    },
-
-    [state, reducer]
-  );
-  return { state, dispatch };
+      runSideEffects();
+    }
+  }, [sideEffects]); //eslint-disable-line
+  return [state, dispatch];
 }
